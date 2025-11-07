b8191bf6e13b6e6b8154723bf3864461c2e7aad2
Paweł Sumiński <p.suminski@gmail.com>
2025-11-07 21:41:52 +0100

    filter

--- src/resources/js/components/frontend/category/Filters/Filter.vue
+++ src/resources/js/components/frontend/category/Filters/Filter.vue
@@ -50,7 +50,7 @@
                 <IconChevron down/>
             </span>
         </legend>
-        <div class="filter-options sm:absolute sm:top-full sm:z-10" v-show="isOpened">
+        <div class="filter-options sm:absolute sm:top-full sm:z-10 sm:min-w-[300px]" v-show="isOpened">
             <div>
                 <slot/>
             </div>
